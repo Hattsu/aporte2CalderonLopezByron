91e7167f60bfad98e4ff11348b3e9ae4a45b1de8
Byron Calderón <102260446+Hattsu@users.noreply.github.com>
2022-07-29 20:31:07 -0500

    Delete Parlante.cs

--- Parlante.cs
+++
@@ -1,9 +0,0 @@
-public class Parlante: Decorador{
-
-    public Parlante(OrdenBase ordenBase):base(ordenBase){ }
-
-    public override string hardware(){
-        var teclado = "El hardware llamado PARLANTE se ha agregado";
-        return teclado;
-    }
-}
