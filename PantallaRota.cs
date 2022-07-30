7ef7c069dab0c54a4eaefdac7d5349d71d9ca63b
Byron Calderón <102260446+Hattsu@users.noreply.github.com>
2022-07-29 20:30:58 -0500

    Delete PantallaRota.cs

--- PantallaRota.cs
+++
@@ -1,8 +0,0 @@
-public class PantallaRota: Decorador{
-
-    public PantallaRota(OrdenBase ordenBase):base(ordenBase){ }
-    public virtual string reparar_dispositivo(){
-        var reparar = "Reparando pantalla rota del dispositivo... completo";
-          return reparar;
-    }
-}
