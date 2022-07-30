4f6b2eed0303d2f89d29cd0eb3d3a2011cc27f5c
Byron Calderón <102260446+Hattsu@users.noreply.github.com>
2022-07-29 20:28:57 -0500

    Delete Agua.cs

--- Agua.cs
+++
@@ -1,8 +0,0 @@
-public class Agua: Decorador{
-
-    public Agua(OrdenBase ordenBase):base(ordenBase){ }
-    public virtual string reparar_dispositivo(){
-        var reparar = "Reparando por daños de agua del dispositivo... completo";
-          return reparar;
-    }
-}
