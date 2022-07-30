1a8e99e0695d3a45b538e838fc7140ab54db7387
Byron Calderón <102260446+Hattsu@users.noreply.github.com>
2022-07-29 20:30:41 -0500

    Delete Formateo.cs

--- Formateo.cs
+++
@@ -1,8 +0,0 @@
-public class Formateo: Decorador{
-
-    public Formateo(OrdenBase ordenBase):base(ordenBase){ }
-    public virtual string reparar_dispositivo(){
-        var reparar = "Reparando por formateo del dispositivo... completo";
-          return reparar;
-    }
-}
